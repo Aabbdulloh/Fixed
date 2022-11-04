5663634f34bbff5348fc0af96dbc3d2086d8a530
Aabbdulla <figmafigma6@gmail.com>
2022-11-04 22:12:48 +0500

    ccc

---
+++ .idea/2.py
@@ -0,0 +1,30 @@
+# S1 = ["olma", "anor", "uzum","olcha","behi","o`rik"]
+# S2 = ["olma", "anor", "mandarin","olcha","behi","malina"]
+# d=0
+# for i in S1:
+#     for j in S2:
+#         if j in i:
+#             d+=1
+# print(d)
+
+# S1 = ["olma", "anor", "uzum","olcha","behi","o`rik"]
+# S2 = ["olma", "anor", "mandarin","olcha","behi","malina"]
+# S3 = []
+# for i in S1:
+#     for j in S2:
+#         if j in i:
+#             S3.append(i)
+# print(S3)
+
+S1 = "Abdulla Bekzod Xasan Ibrohim Shahzod Jaloliddin"
+s=[]
+d=[]
+k = S1.split()
+for i in k:
+    if i!=k:
+       s.append(len(i))
+    print(d)
+
+
+
+
